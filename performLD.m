70cb83334e44d3fdde45cb5175846fb794501f8b
pengqiannan <116168351+pengqiannan@users.noreply.github.com>
2024-09-01 11:56:00 +0800

    Update performLD.m

--- performLD.m
+++ performLD.m
@@ -13,5 +13,5 @@ directory = 'F:\model\';
 compoundtest = {'cpd00013','cpd00027','cpd00197','cpd00077','cpd00401','cpd00153','cpd04073','cpd00561','cpd00489','cpd00604','cpd00136','cpd01059','cpd01733'};;
 scenarioID = 4;
 syntheticCell = 0;
-[BioAndDegradationAll, modelsAll, mediumAll, Singlemodels, superModel] = superCCmain (scenarioID,directory,filenames, speciesToConsider,mmMedium, CNsourcesName, CNsourcesNumber,compoundtest,PercentOfSpeciesBio,syntheticCell);
+[BioAndDegradationAll, modelsAll, mediumAll, Singlemodels, superModel] = LDmain (scenarioID,directory,filenames, speciesToConsider,mmMedium, CNsourcesName, CNsourcesNumber,compoundtest,PercentOfSpeciesBio,syntheticCell);
 save('PVAS.mat')
